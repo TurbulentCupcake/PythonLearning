1e405fa81f25ab025e3ac1832ed94976b579fb05
Adithya Murali <adithya97@live.com>
2016-08-06 01:58:21 +0400

    functions to store names

---
+++ MyNames.py
@@ -0,0 +1,26 @@
+def init(data):
+	data['first'] = {}
+	data['second'] = {}
+	data['last'] = {}
+
+
+def lookup(data, label, name):
+	return data[label].get(name)
+
+def store(data, fullname):
+	names = fullname.split()
+	if len(names) == 1: 
+		names.insert(1, "")
+		names.insert(2, "")
+	elif len(names) == 2:
+		names.insert(1, "")
+
+	labels = ['first','second','last']
+
+	for label, name in zip(labels, names):
+		people = lookup(data, label, name)
+		if people:
+			people.append(fullname)
+		else:
+			data[label][name] = [fullname]
+
