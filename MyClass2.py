f5d64742a01a6e00254944ebe46d1ca5a7789536
Adithya Murali <adithya97@live.com>
2016-08-07 21:20:46 +0400

    Birdie Talk

---
+++ MyClass2.py
@@ -0,0 +1,18 @@
+class Bird:
+	def __init__(self):
+		self.hungry = True
+	def eat(self):
+		if self.hungry:
+			print 'Aaaah...'
+			self.hungry = False
+		else:
+			print 'No, thanks!'
+
+
+class SongBird(Bird):
+	def __init__(self):
+		self.sound = 'Squawk!'
+	def sing(self):
+		print self.sound
+
+		
